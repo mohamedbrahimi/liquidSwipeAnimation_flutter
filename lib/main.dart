8f78d90731a4620679dcf7aa4027b630db140b88
mohamedbrahimi <mohkriss3@gmail.com>
2020-01-12 21:50:06 +0100

    defined third page

--- lib/main.dart
+++ lib/main.dart
@@ -134,8 +134,57 @@ class MyApp extends StatelessWidget {
         ],
       ),
     ),
-    Container(),
-    Container(),
+    Container(
+      color: Colors.orange,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  "GoldCoin",
+                  style: goldCoinWhiteStyle,
+                ),
+                Text(
+                  "Skip",
+                  style: goldCoinWhiteStyle,
+                )
+              ],
+            ),
+          ),
+          Image.asset("assets/img/firstImage.png"),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Online",
+                  style: whiteStyle,
+                ),
+                Text(
+                  "Gambling",
+                  style: boldStyle,
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  "Temporibus autem aut\n"
+                      "officiels debits aut rerun \n"
+                      "necessitatibus",
+                  style: descriptionWhiteStyle,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    ),
   ];
 
   @override
